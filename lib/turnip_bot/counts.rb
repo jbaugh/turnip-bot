07503fdf7922daa0ae33f166d541d69e5178db33
Jarrett Baugh <jarrett.baugh@gmail.com>
2020-03-28 19:50:25 -0700

    Basic working example of keeping state, but not persisted

--- lib/turnip_bot/counts.rb
+++ lib/turnip_bot/counts.rb
@@ -1,3 +1,5 @@
+require 'tzinfo'
+
 module TurnipBot
   class Counts
     attr_reader :counts
@@ -7,11 +9,42 @@ module TurnipBot
     end
 
     def add(timestamp, author, count)
-      time = timestamp.strftime('%m-%d %H:%M')
+      timestamp = convert_to_local_time(timestamp)
 
+      count_key = get_counts_key(timestamp)
 
+      @counts[count_key] ||= {}
+      @counts[count_key][author] = count
 
-      puts "#{time} :: #{author} has recorded a count of #{count}!"
+      puts "#{timestamp.strftime('%m-%d %H:%M')} :: #{author} has recorded a count of #{count}!"
+    end
+
+    def current_counts
+      key = get_counts_key(convert_to_local_time(Time.now))
+      counts[key] || {} 
+    end
+
+    private
+
+    def get_counts_key(time)
+      date_string(time) + "_" + time_of_day(time)
+    end
+
+    def time_of_day(time)
+      if time.hour >= 12
+        'evening'
+      else
+        'morning'
+      end
+    end
+
+    def date_string(time)
+      time.strftime('%m-%d')
+    end
+
+    def convert_to_local_time(time)
+      tz = TZInfo::Timezone.get('America/Los_Angeles')
+      tz.to_local(time)
     end
   end
 end
--- lib/turnip_bot/handler.rb
+++ lib/turnip_bot/handler.rb
@@ -16,19 +16,36 @@ module TurnipBot
         puts
       end
 
+      # This seems to be matching for every message, so the starts_with logic isnt behaving how we expect
       bot.message(starts_with: /\d{2,}/) do |event|
         val = event.content.match(/\A\d{2,}/)
         if val
           turnip_count = val[0]
           author = event.author.username
 
-          counts.add(event.timestamp, author, turnip_count) 
+          counts.add(event.timestamp, author, turnip_count)
+
         else
-          puts "@"*100
-          puts "Error processing event"
-          puts event.inspect
-          puts "@"*100
+          # puts "@"*100
+          # puts "Error processing event"
+          # puts event.inspect
+          # puts "@"*100
         end
+      end
+
+      bot.message(with_text: '!turnip') do |event|
+        current_counts = counts.current_counts
+        if current_counts.empty?
+          message = "There are no turnip prices :("  
+        else
+          messages = ["The most recent prices are:"]
+          counts.current_counts.each do |author, price|
+            messages << "  #{author} -> #{price}"
+          end
+          message = messages.join("\n")
+        end
+
+        event.respond message
       end
 
       bot.message(with_text: 'turnip') do |event|
